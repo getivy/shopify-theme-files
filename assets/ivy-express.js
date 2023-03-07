ef6c9ef82f3da413f1378d2faa978bd6a24b2db2
Jonas Körber <86711125+jonas-netshake@users.noreply.github.com>
2023-03-07 12:48:31 +0100

    init

---
+++ assets/ivy-express.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expressButtons = document.querySelectorAll("button.ivy-express-checkout");
+
+  expressButtons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+
+      let fetchUrl = null;
+
+      if(button.dataset.type === 'cart') {
+        fetchUrl = "/cart?view=ivy";
+      } else if(button.dataset.type === 'product' && button.dataset.product && button.dataset.shopifyVariant) {
+        fetchUrl = "/products/" + button.dataset.product + "?variant=" + button.dataset.shopifyVariant + "&view=ivy";
+      }
+
+      if(fetchUrl !== null) {
+        fetch(fetchUrl)
+        .then((response) => response.json())
+        .then((cartData) => {
+          fetch('https://www.netshake.io/ivy/express/createCheckoutSession.php', {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(cartData),
+          })
+          .then((response) => response.json())
+          // .then((checkoutSessionData) => { window.location.href = checkoutSessionData.redirectUrl; })
+          .then((checkoutSessionData) => { startIvyCheckout(checkoutSessionData.redirectUrl, 'popup'); })
+          .catch((error) => console.error("Error:", error));
+        });
+
+      };
+
+    });
+
+  });
+
+});
